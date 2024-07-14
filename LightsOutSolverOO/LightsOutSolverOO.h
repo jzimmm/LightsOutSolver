769deab11bb49627dcb48d6e55cc5f923d90c5fa
jzimmm <jordan.zimmerman26@gmail.com>
2024-07-13 22:27:36 -0400

    Removed unnecessary header files.

--- LightsOutSolverOO/LightsOutSolverOO.h
+++ LightsOutSolverOO/LightsOutSolverOO.h
@@ -7,17 +7,13 @@
 
 
 // C RunTime Header Files:
-#include <stdlib.h>
 #include <malloc.h>
-#include <memory.h>
 #include <wchar.h>
 #include <math.h>
-#include <strsafe.h>
 
 #include <d2d1.h>
 #include <d2d1helper.h>
 #include <dwrite.h>
-#include <wincodec.h>
 #pragma comment(lib, "d2d1.lib")
 #include <dwrite.h>
 #pragma comment(lib, "dwrite.lib")
